f5c7774f5dd6e218234e7f90741dfb9330b5a8b4
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-05-12 12:45:05 +0200

    Uitwerking Novi-opdracht

---
+++ src/components/metricSlider/MetricSlider.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState, useContext } from 'react';
+import './MetricSlider.css';
+import { TempContext } from "../../context/TempContextProvider";
+
+const MetricSlider = () => {
+    const [checked, toggleChecked] = useState(true);
+    const { toggleTemp } = useContext(TempContext);
+
+    useEffect(() =>{
+        toggleTemp();
+    }, [checked]);
+
+
+
+    return (
+
+        <div className="weather-container-extention">
+            Weergeven in
+
+            <p className="switch-label">
+                C &deg;
+            </p>
+
+            <span className="switch-wrapper">
+        <input
+            type="checkbox"
+            className="switch"
+            id="metric-system"
+            checked={checked}
+            onChange={() => toggleChecked(!checked)}
+        />
+
+        <label
+            htmlFor="metric-system"
+            className="switch-btn"
+        />
+      </span>
+
+            <p className="switch-label">
+                &deg; F
+            </p>
+        </div>
+    );
+}
+
+export default MetricSlider;
